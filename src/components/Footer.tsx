3ffee18cefdc54c3e0de3e4fc7cca1c1d8408d3a
abi <abikandiah@gmail.com>
2025-10-30 23:12:06 -0400

    Added disclaimer to footer + added .text-link style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,24 @@
+import DisclaimerUrl from "@/assets/disclaimer.txt";
+
 
 function Footer() {
     return (
         <footer className="mt-auto px-3 xl:px-0">
-            <div className="py-16 px-3 border-black border">
+            <div className="py-16 px-3">
 
-                <div className="flex md:justify-end justify-center">
 
-                    <p className="text-sm text-gray-500">
+                <div className="flex flex-col md:items-end items-center gap-1">
+                    <a className="text-sm text-link" href={DisclaimerUrl}
+                        target="_blank" rel="noopener noreferrer"
+                    >
+                        View disclaimer
+                    </a>
+
+                    <span className="text-sm text-gray-500">
                         ©
                         2025
                         Abilaesh Kandiah. All rights reserved.
-                    </p>
+                    </span>
                 </div>
             </div>
         </footer>
--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -5,7 +5,7 @@ export function TextLink({ className, ...props }: LinkComponentProps) {
     return (
         <Link
             {...props}
-            className={cn("text-blue-600 hover:text-blue-800 visited:text-blue-900", className)} />
+            className={cn("text-link", className)} />
     )
 }
 
