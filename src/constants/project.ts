275f34a5d43896ef3884ee4fa3e53d97d415c61a
abi <abikandiah@gmail.com>
2025-11-01 00:58:28 -0400

    renaming

--- src/constants/project.ts
+++ src/constants/project.ts
@@ -1,13 +1,13 @@
-import { automatedTranslationsProject } from "@/components/projects/AutomatedTranslations";
-import { dataUploadProject } from "@/components/projects/DataUpload";
-import { javaToReactFormBuilderProject } from "@/components/projects/form-builder/ReactFormBuilder";
-import { googleVaultProject } from "@/components/projects/GoogleVaultCollector";
-import { legalHoldNotificationsProject } from "@/components/projects/LegalHoldNotifications";
-import { microsoftEDiscoveryProject } from "@/components/projects/MicrosoftEDiscoveryCollector";
+import { automatedTranslationsProject as automatedTranslations } from "@/components/projects/AutomatedTranslations";
+import { dataUploadProject as dataUpload } from "@/components/projects/DataUpload";
+import { javaToReactFormBuilderProject as javaToReactFormBuilder } from "@/components/projects/form-builder/ReactFormBuilder";
+import { googleVaultProject as googleVaultCollector } from "@/components/projects/GoogleVaultCollector";
+import { legalHoldNotificationsProject as legalHoldNotifications } from "@/components/projects/LegalHoldNotifications";
+import { microsoftEDiscoveryProject as microsoftEDiscoveryCollector } from "@/components/projects/MicrosoftEDiscoveryCollector";
 import { jobSchedulerWebApp } from "@/components/projects/JobSchedulerWebApp";
-import { selenumE2ETestSuiteProject } from "@/components/projects/SeleniumE2ETestSuite";
-import { thirdPartyServicesProject } from "@/components/projects/ThirdPartyServicesFramework";
-import { webPortfolioProject } from "@/components/projects/WebPortfolio";
+import { selenumE2ETestSuiteProject as selenumE2ETestSuite } from "@/components/projects/SeleniumE2ETestSuite";
+import { thirdPartyServicesProject as thirdPartyServices } from "@/components/projects/ThirdPartyServicesFramework";
+import { webPortfolioProject as webPortfolio } from "@/components/projects/WebPortfolio";
 import { Project, type ProjectProps } from "@/types/ProjectTypes";
 
 const projectsMap: Map<string, Project> = new Map();
@@ -19,15 +19,15 @@ function addProject(props: ProjectProps) {
 }
 
 addProject(jobSchedulerWebApp);
-addProject(legalHoldNotificationsProject);
-addProject(javaToReactFormBuilderProject);
-addProject(thirdPartyServicesProject);
-addProject(googleVaultProject);
-addProject(microsoftEDiscoveryProject);
-addProject(dataUploadProject);
-addProject(selenumE2ETestSuiteProject);
-addProject(automatedTranslationsProject);
-addProject(webPortfolioProject);
+addProject(legalHoldNotifications);
+addProject(javaToReactFormBuilder);
+addProject(thirdPartyServices);
+addProject(googleVaultCollector);
+addProject(microsoftEDiscoveryCollector);
+addProject(dataUpload);
+addProject(selenumE2ETestSuite);
+addProject(automatedTranslations);
+addProject(webPortfolio);
 
 
 const projects = Array.from(projectsMap.values());
