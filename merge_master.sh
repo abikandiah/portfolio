b0dee215253b18965734495176ed63ae9c28e1a5
abi <abikandiah@gmail.com>
2025-11-09 00:46:54 -0500

    updated merge_master

--- merge_master.sh
+++ merge_master.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
+
+check_prereqs() {
+	if [[ -n $(git status --porcelain --untracked-files=no) ]]; then
+		echo "Current branch has uncommitted files."
+		git status
+		return 1;
+	else
+		return 0;
+	fi
+}
+
+
+# Call the function and check its exit status
+check_prereqs
+if [ $? -ne 0 ]; then
+  echo "Quitting script due to missing prerequisites."
+  exit 1
+fi
+
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+echo Merging $current_branch into master
+
 git checkout master
-git merge dev
+git merge $current_branch
 git push
+git checkout $current_branch
+
+
 
 
